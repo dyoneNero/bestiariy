1fcdf87d7e6e803731b4e3216f1d56c94c44c712
dyoneNero <dyonenero@gmail.com>
2022-05-24 02:57:53 +0300

    best

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+import React, {Suspense} from "react";
+import Anverbook from "./component/Anverbook"
+import Page1 from './image/list/page-1';
+import Page2 from './image/list/page-2';
+import Page3 from './image/list/page-3';
+import Page4 from './image/list/page-4';
+import Page5 from './image/list/page-5';
+import Page6 from './image/list/page-6';
+import Page7 from './image/list/page-7';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return (
+        <div className="App">
+            <div className="app__page-title">
+                <p>БЕСТИАРИЙ</p>
+            </div>
+            <div className="app__page-info">
+                <p>Наша работа является уникальным проектом.Все монстры вымышлены и индивидуальны.</p>
+            </div>
+            <div className="app__page-3d">
+                <div className="container">
+                    <p>Здесь вы можете лицезреть бестиарий в ЗD и 2D визуализации.</p>
+                </div>
+            </div>
+            <div className="app__page-book3d">
+                <Canvas className="canvas">
+                    <OrbitControls enableZoom={false}/>
+                    <ambientLight intensity={0.5}/>
+                    <directionalLight position={[-2, 5, 2]} intensity={1}/>
+                    <Suspense fallback="null">
+                        <Anverbook/>
+                    </Suspense>
+                </Canvas>
+            </div>
+            <div className="app__page-2d">
+                <div className="container">
+                    <div className="list">
+                        <img src={Page1} alt=""/>
+                    </div>
+                    <div className="list">
+                        <img src={Page2} alt=""/>
+                    </div>
+                    <div className="list">
+                        <img src={Page3} alt=""/>
+                    </div>
+                    <div className="list">
+                        <img src={Page4} alt=""/>
+                    </div>
+                    <div className="list">
+                        <img src={Page5} alt=""/>
+                    </div>
+                    <div className="list">
+                        <img src={Page6} alt=""/>
+                    </div>
+                    <div className="list">
+                        <img src={Page7} alt=""/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
